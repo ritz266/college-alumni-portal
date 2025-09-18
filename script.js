a6d27f12367a8be92ebdea15adae16427d6ebe2d
ritz266 <101943320+ritz266@users.noreply.github.com>
2025-09-18 11:07:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbzbbxJiDL-9hYOI5UDjXOnT92yFEwJ1TPxDqfQDyIbJSW8op9wYS1qVJYrwkCH_aOEU/exec";
-
 async function registerUser(data) {
   const res = await fetch(API_URL, {
     method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ action: "register", ...data })
   });
   return await res.json();
@@ -11,6 +11,7 @@ async function registerUser(data) {
 async function loginUser(data) {
   const res = await fetch(API_URL, {
     method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ action: "login", ...data })
   });
   return await res.json();
@@ -19,6 +20,7 @@ async function loginUser(data) {
 async function addAlumni(data) {
   const res = await fetch(API_URL, {
     method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ action: "addAlumni", ...data })
   });
   return await res.json();
