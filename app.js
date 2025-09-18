c59c157d668c2b331464d29de01dd1b3a9157e1f
ritz266 <101943320+ritz266@users.noreply.github.com>
2025-09-18 11:38:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const backendUrl = "https://script.google.com/macros/s/AKfycbw_Gi31GJB_QQz0LCNHkfKsuLO3FP6nb04LaqWGWeOlgY5KtthJvhwrKGzuXsg_kOPVNg/exec";
+const backendUrl = "https://script.google.com/macros/s/AKfycbyhSWI5LLMLh7RSGtjh2OiI7YEPdHcEJWQa75-yW4Xm4SqnDL2U2vxeXUEt1y4BA59sFA/exec";
 // Replace with your deployed Google Cloud Function endpoint
 
 // LOGIN
